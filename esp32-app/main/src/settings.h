b42661769d34f56447d51ebe9d2c83c4349ce86a
Yoshinari Gyu <50069930+yngyu@users.noreply.github.com>
2022-05-05 18:21:00 +0900

    bme 680 用の原型まで作成

---
+++ esp32-app/main/src/settings.h
@@ -0,0 +1,14 @@
+#ifndef SETTINGS_H_
+#define SETTINGS_H_
+
+#define I2C_MASTER_SCL_IO         (19)
+#define I2C_MASTER_SDA_IO         (18)
+
+#define I2C_MASTER_NUM            (0)
+
+#define I2C_MASTER_FREQ_HZ        (100000)
+#define I2C_MASTER_TX_BUF_DISABLE (0)
+#define I2C_MASTER_RX_BUF_DISABLE (0)
+#define I2C_MASTER_TIMEOUT_MS     (1000)
+
+#endif
