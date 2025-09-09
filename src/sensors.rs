4d11fe66245222d3847b9d4403b0e21b194938d9
Yoshinari Gyu <50069930+yngyu@users.noreply.github.com>
2025-09-10 00:12:40 +0900

    Convert None when Err

--- src/sensors.rs
+++ src/sensors.rs
@@ -2,6 +2,7 @@ use bme680::{
     Bme680, FieldData, IIRFilterSize, OversamplingSetting, PowerMode::ForcedMode, SettingsBuilder,
 };
 use linux_embedded_hal::{Delay, I2CError, I2cdev, Serial};
+use log::error;
 
 use crate::mh_z19c::MhZ19c;
 
@@ -11,10 +12,10 @@ pub struct Sensors {
 }
 
 pub struct Measurements {
-    pub temperature_celsius: f32,
-    pub pressure_pa: f32,
-    pub humidity_percent: f32,
-    pub co2_ppm: u16,
+    pub temperature_celsius: Option<f32>,
+    pub pressure_pa: Option<f32>,
+    pub humidity_percent: Option<f32>,
+    pub co2_ppm: Option<u16>,
 }
 
 impl Sensors {
@@ -46,23 +47,17 @@ impl Sensors {
         }
     }
 
-    pub async fn measure(&mut self) -> Result<Measurements, Box<dyn std::error::Error>> {
-        let bme680_data = self
-            .measure_bme680()
-            .expect("Error has happened during getting data");
+    pub async fn measure(&mut self) -> Measurements {
+        let bme680_data = self.measure_bme680();
+        if bme680_data.is_err() {
+            error!("{:?}", bme680_data.as_ref().unwrap_err());
+        }
+        let co2_ppm = self.mh_z19c.read_co2().await;
+        if co2_ppm.is_err() {
+            error!("{:?}", co2_ppm.as_ref().unwrap_err());
+        }
 
-        let co2_ppm = self.mh_z19c.read_co2().await?;
-
-        let measurements = Measurements {
-            temperature_celsius: bme680_data.temperature_celsius(),
-            pressure_pa: bme680_data.pressure_hpa() * 100.0,
-            humidity_percent: bme680_data.humidity_percent(),
-            co2_ppm,
-        };
-
-        self.verify_data(&measurements)?;
-
-        Ok(measurements)
+        self.verify_data(&bme680_data, &co2_ppm)
     }
 
     fn measure_bme680(&mut self) -> Result<FieldData, bme680::Error<I2CError>> {
@@ -74,20 +69,60 @@ impl Sensors {
         Ok(data)
     }
 
-    fn verify_data(&self, measurements: &Measurements) -> Result<(), Box<dyn std::error::Error>> {
-        if measurements.temperature_celsius < -40.0 || 85.0 < measurements.temperature_celsius {
-            return Err("The temperature data is invalid.".into());
-        }
-        if measurements.pressure_pa < 30000.0 || 110000.0 < measurements.pressure_pa {
-            return Err("The air pressure data is invalid.".into());
-        }
-        if measurements.humidity_percent < 0.0 || 100.0 < measurements.humidity_percent {
-            return Err("The humidity data is invalid.".into());
-        }
-        if measurements.co2_ppm < 300 || 10000 < measurements.co2_ppm {
-            return Err("The co2 data is invalid.".into());
-        }
+    fn verify_data(
+        &self,
+        bme680_data: &Result<FieldData, bme680::Error<I2CError>>,
+        co2_ppm: &Result<u16, Box<dyn std::error::Error>>,
+    ) -> Measurements {
+        let temperature_celsius = if bme680_data.is_ok() {
+            let temp = bme680_data.as_ref().unwrap().temperature_celsius();
+            if (-40.0..=85.0).contains(&temp) {
+                Some(temp)
+            } else {
+                None
+            }
+        } else {
+            None
+        };
 
-        Ok(())
+        let pressure_pa = if bme680_data.is_ok() {
+            let pressure = bme680_data.as_ref().unwrap().pressure_hpa() * 100.0;
+            if (30000.0..=110000.0).contains(&pressure) {
+                Some(pressure)
+            } else {
+                None
+            }
+        } else {
+            None
+        };
+
+        let humidity_percent = if bme680_data.is_ok() {
+            let humidity = bme680_data.as_ref().unwrap().humidity_percent();
+            if (0.0..=100.0).contains(&humidity) {
+                Some(bme680_data.as_ref().unwrap().humidity_percent())
+            } else {
+                None
+            }
+        } else {
+            None
+        };
+
+        let co2_ppm = if co2_ppm.is_ok() {
+            let co2_ppm = *co2_ppm.as_ref().unwrap();
+            if (300..=10000).contains(&co2_ppm) {
+                Some(co2_ppm)
+            } else {
+                None
+            }
+        } else {
+            None
+        };
+
+        Measurements {
+            temperature_celsius,
+            pressure_pa,
+            humidity_percent,
+            co2_ppm,
+        }
     }
 }
